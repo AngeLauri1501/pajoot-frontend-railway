b061ff2368b049a6415eba31db9fbd12c559fcae
AngeLauri1501 <126652766+AngeLauri1501@users.noreply.github.com>
2024-04-16 15:07:16 +0200

    Update playerTimeUp.jsx

--- src/components/playerTimeUp.jsx
+++ src/components/playerTimeUp.jsx
@@ -6,6 +6,9 @@ import Gongsound from '../../public/assets/sounds/Gong.mp3';
 const PlayerTimeUp = () => {
   const { game, answeredCorrectly, question, userLogged, muted, mensajeRacha } = useStore();
   const audioRef = useRef(false);
+  const players = game.gameData.players.players;
+  // Ordenar los jugadores por puntuación
+  const sortedPlayers = players.sort((a, b) => b.gameData.score - a.gameData.score);
 
   useEffect(() => {
     if (audioRef.current) {
@@ -14,14 +17,32 @@ const PlayerTimeUp = () => {
   }, [audioRef]);
 
   return (
-    <>
-      <p className='time-out'>Se ha acabado el tiempo</p>
+    <div className='remote-timeout'>
+      
       {game && game.remoteMode && !muted && (
         <audio id='timeup-music' src={Gongsound} autoPlay ref={audioRef} />
       )}
+      {game && game.remoteMode &&(  
+        <div className='question-container ranking-container'>
+            <div className='ranking-content'>
+              <h1>Clasificación</h1>
+              <ul className='ranking-list'>
+              {/* Mapear y renderizar los jugadores en orden de puntuación */}
+              {sortedPlayers.map((player, index) => (
+              <li className='ranking-player' key={player.playerId}>
+              <p>{index + 1}.</p>
+              <img className='ranking-image' src={player.photo} alt={player.playerName} />
+              {player.name}: {(player.gameData.score).toFixed()}
+              </li>
+              ))}
+              </ul>
+            </div>
+        </div>
+      )}
+      <div>
       <div className='question-container answer-container'>
 
-        <h1>Respuesta {answeredCorrectly ? "Correcta!" : "Incorrecta!"}</h1>
+        <h1>¡Respuesta {answeredCorrectly ? "Correcta!" : "Incorrecta!"}</h1>
         {answeredCorrectly ? (
           <img src='./assets/gif/cat-yes.gif' className='answer-image' alt='Gato asintiendo' />
         ) : (
@@ -41,7 +62,8 @@ const PlayerTimeUp = () => {
           <p>{mensajeRacha}</p>
         </div>
       )}
-    </>
+      </div>
+    </div>
   );
 };
 
